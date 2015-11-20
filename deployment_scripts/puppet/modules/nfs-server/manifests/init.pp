23ec08b948cab8b903f7937a2d62f9d63060a860
srudyka <sergey.rudyka@gmail.com>
2015-11-20 10:50:04 +0200

    updated init and params

--- deployment_scripts/puppet/modules/nfs-server/manifests/init.pp
+++ deployment_scripts/puppet/modules/nfs-server/manifests/init.pp
@@ -1,22 +1,24 @@
 
 class nfs-server {
-  $path = '/nfs'
-  package { 'nfs-kernel-server':  
-  ensure => installed, 
+  include nfs_server::params
+
+  $path = $nfs_server::params::nfs_root_path
+  package { '$nfs_server::params::pkg_name_server':  
+    ensure => installed, 
           }
   file { 'nfs_dir':
-  path    => '/nfs',
-  ensure  => directory,
+    path    => $path,
+    ensure  => directory,
        }
   file { 'exports':
-  path    => '/etc/exports',
-  ensure  => present,
-  content => "$path *(rw,sync,no_root_squash,no_subtree_check)\n",
-  notify  => Service[nfs-kernel-server],
+    path    => '/etc/exports',
+    ensure  => present,
+    content => "$path *(rw,sync,no_root_squash,no_subtree_check)\n",
+    notify  => Service[$nfs_server::params::service_name],
       }
-  service { nfs-kernel-server:
-  ensure    => running,
-  enable    => true,
-    }
+  service { $nfs_server::params::service_name:
+    ensure    => running,
+    enable    => true,
+          }
 }
 
---
+++ deployment_scripts/puppet/modules/nfs-server/manifests/params.pp
@@ -0,0 +1,38 @@
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class nfs-server::params {
+
+  $nfs-server_hash = hiera('nfs-server')
+  $nfs_root_path   = $nfs-server_hash['export_path']
+
+  case $::operatingsystem {
+    'Ubuntu', 'Debian': {
+      $pkg_name_server      = 'nfs-kernel-server'
+      $pkg_name_client      = 'nfs-common'
+      $service_name         = 'nfs-kernel-server'
+    }
+    'CentOS', 'RedHat': {
+
+      $pkg_name_server      = 'nfs-kernel-server'
+      $pkg_name_client      = 'nfs-common'
+      $service_name         = 'nfs'
+    }
+    default: {
+      fail("unsuported osfamily ${::osfamily}, currently Debian and Redhat are the only supported platforms")
+    }
+  }
+
+}
