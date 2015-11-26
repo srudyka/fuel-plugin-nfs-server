44229ad62b175078cc3549ff4e244400364385f6
srudyka <sergey.rudyka@gmail.com>
2015-11-26 13:55:18 +0200

    puppet-lint

--- deployment_scripts/puppet/modules/nfs_server/manifests/install.pp
+++ deployment_scripts/puppet/modules/nfs_server/manifests/install.pp
@@ -3,17 +3,17 @@ class nfs_server::install  {
   include nfs_server::firewall
 
   package { $nfs_server::params::pkg_name_server:
-    ensure => installed, 
+    ensure => installed,
   }
   file { 'nfs_dir':
-    path    => $nfs_server::params::nfs_root_path,
     ensure  => directory,
+    path    => $nfs_server::params::nfs_root_path,
   }
   file { 'exports':
-    path    => '/etc/exports',
     ensure  => present,
-    content => "$nfs_server::params::nfs_root_path *(rw,sync,no_root_squash,no_subtree_check)\n",
-    notify  => Service["$nfs_server::params::service_name"],
+    path    => '/etc/exports',
+    content => "${nfs_server::params::nfs_root_path} *(rw,sync,no_root_squash,no_subtree_check)\n",
+    notify  => Service[$nfs_server::params::service_name]
   }
   service { $nfs_server::params::service_name:
     ensure    => running,
